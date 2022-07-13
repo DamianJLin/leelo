c2fb453b3bede0cfa1d2669db0c2239290183bf5
Damian Lin <damianlin@iinet.net.au>
2022-07-14 03:13:58 +1000

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-use std::f64;
 use csv::Reader;
 use csv::Writer;
+use std::cmp;
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
+use std::error::Error;
+use std::f64;
 use std::io;
 use std::io::Write;
-use std::collections::hash_map::Entry;
-use std::error::Error;
-use std::cmp;
 
 const INITIAL_RATING: f64 = 1000.;
 // RATING_CONST determines how a expected_win_probability is inferred from a difference in rating.
@@ -17,14 +17,18 @@ const K: f64 = 40.; // Rating sensitivity (max. rating change from a single game
 enum MatchResult {
     WhiteWin,
     BlackWin,
-    Draw
+    Draw,
 }
 
 enum Operation {
     Help,
     New,
     AddPlayer(String),
-    Update { white_player_id: String, black_player_id: String, result: MatchResult},
+    Update {
+        white_player_id: String,
+        black_player_id: String,
+        result: MatchResult,
+    },
     View,
 }
 
@@ -39,23 +43,24 @@ impl Config {
             return Err("not enough arguments. Try leelo help.");
         }
         if args[1] == "help" {
-            return Ok(Config { filename: "".to_string(), operation: Operation::Help });
+            return Ok(Config {
+                filename: "".to_string(),
+                operation: Operation::Help,
+            });
         }
 
         let filename = args[1].clone();
         let command = args[2].clone();
-        
+
         let operation = match command.as_str() {
             "help" | "h" => Operation::Help,
-            "new" |"n" => {
-                Operation::New
-            }                    
+            "new" | "n" => Operation::New,
             "player" | "p" => {
                 if args.len() < 4 {
                     return Err("not enough arguments for this command.");
                 }
-                Operation::AddPlayer(args[3].clone()) 
-            },
+                Operation::AddPlayer(args[3].clone())
+            }
             "game" | "g" => {
                 if args.len() < 6 {
                     return Err("not enough arguments for this command.");
@@ -64,21 +69,22 @@ impl Config {
                     "1-0" => MatchResult::WhiteWin,
                     "0-1" => MatchResult::BlackWin,
                     "0.5-0.5" => MatchResult::Draw,
-                    _ => return Err("unable to interpret match_result argument.")
+                    _ => return Err("unable to interpret match_result argument."),
                 };
                 Operation::Update {
                     white_player_id: args[3].clone(),
                     black_player_id: args[4].clone(),
-                    result: result
+                    result: result,
                 }
             }
-            "view" | "v" => {
-                Operation::View
-            }
+            "view" | "v" => Operation::View,
             _ => return Err("unknown command. Try leelo help."),
         };
 
-        Ok(Config { filename, operation })
+        Ok(Config {
+            filename,
+            operation,
+        })
     }
 }
 
@@ -108,31 +114,34 @@ fn write_to_csv(filename: &str, data: &mut HashMap<String, f64>) -> Result<(), B
 
 fn create_player(player_id: String, data: &mut HashMap<String, f64>) -> Result<(), Box<dyn Error>> {
     match (*data).entry(player_id) {
-        Entry::Occupied(_) => {
-           return Err("player_id already in use.".into()) 
-        }
+        Entry::Occupied(_) => return Err("player_id already in use.".into()),
         Entry::Vacant(v) => {
-           v.insert(INITIAL_RATING);
+            v.insert(INITIAL_RATING);
         }
     }
 
     Ok(())
 }
 
-fn update_ratings(white_player_id: String, black_player_id: String, result: MatchResult, data: &mut HashMap<String, f64>) -> Result<(), Box<dyn Error>> {
+fn update_ratings(
+    white_player_id: String,
+    black_player_id: String,
+    result: MatchResult,
+    data: &mut HashMap<String, f64>,
+) -> Result<(), Box<dyn Error>> {
     let white_rating = match (*data).get(&white_player_id) {
         Some(rat) => f64::from(*rat),
-        None => return Err("white player not found.".into())
+        None => return Err("white player not found.".into()),
     };
     let black_rating = match (*data).get(&black_player_id) {
         Some(rat) => f64::from(*rat),
-        None => return Err("black player not found.".into())
+        None => return Err("black player not found.".into()),
     };
     let rating_difference = white_rating - black_rating;
     let white_score_expected = 1. / (f64::exp(-rating_difference / RATING_CONST) + 1.);
     let black_score_expected = 1. - white_score_expected;
 
-    let (white_score, black_score) =  match result {
+    let (white_score, black_score) = match result {
         MatchResult::WhiteWin => (1., 0.),
         MatchResult::BlackWin => (0., 1.),
         MatchResult::Draw => (0.5, 0.5),
@@ -142,7 +151,7 @@ fn update_ratings(white_player_id: String, black_player_id: String, result: Matc
     let black_rating_change = K * (black_score - black_score_expected);
     let white_new_rating = white_rating + white_rating_change;
     let black_new_rating = black_rating + black_rating_change;
-    
+
     (*data).insert(white_player_id, white_new_rating);
     (*data).insert(black_player_id, black_new_rating);
 
@@ -171,16 +180,20 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
             let mut data: HashMap<String, f64> = HashMap::new();
             write_to_csv(&config.filename, &mut data)?;
         }
-        Operation::Update{ white_player_id, black_player_id, result } => {
+        Operation::Update {
+            white_player_id,
+            black_player_id,
+            result,
+        } => {
             let mut data: HashMap<String, f64> = HashMap::new();
             read_to_hashmap(&config.filename, &mut data)?;
-            update_ratings(white_player_id, black_player_id, result, &mut data)?; 
+            update_ratings(white_player_id, black_player_id, result, &mut data)?;
             write_to_csv(&config.filename, &mut data)?;
         }
         Operation::AddPlayer(player_id) => {
             let mut data: HashMap<String, f64> = HashMap::new();
             read_to_hashmap(&config.filename, &mut data)?;
-            create_player(player_id, &mut data)?; 
+            create_player(player_id, &mut data)?;
             write_to_csv(&config.filename, &mut data)?;
         }
         Operation::View => {
@@ -197,7 +210,12 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
             for (player_id, rating) in &data_vec {
                 let player_id_len = player_id.len();
                 let tabs = (max_player_id_len - player_id_len) / 8 + 2;
-                print!("{}{}{}\n", player_id, "\t".repeat(tabs), (**rating).round() as u32);
+                print!(
+                    "{}{}{}\n",
+                    player_id,
+                    "\t".repeat(tabs),
+                    (**rating).round() as u32
+                );
                 io::stdout().flush()?;
             }
         }
