c9e22708ee0a228d71469c97a9fb6a799ebbb5b1
Damian Lin <damianlin@iinet.net.au>
2022-07-13 17:16:04 +1000

    Modify helper functions to borrow HashMap. Add create and update players

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::f64;
 use csv::Reader;
 use csv::Writer;
 use std::collections::HashMap;
@@ -5,16 +6,24 @@ use std::collections::hash_map::Entry;
 use std::error::Error;
 
 const INITIAL_RATING: u32 = 200;
+const RATING_CONST: f64 = 400.; // Constant used in calculating probability from rating difference.
+const K: f64 = 20.; // Sensitivity when updating ratings.
 
 pub struct Config {
     pub filename: String,
     operation: Operation,
 }
 
+enum MatchResult {
+    WhiteWin,
+    BlackWin,
+    Draw
+}
+
 enum Operation {
     Help,
     AddPlayer(String),
-    Update { white_player: String, black_player: String, score: String},
+    Update { white_player_id: String, black_player_id: String, result: MatchResult},
     Reset,
 }
 
@@ -35,6 +44,22 @@ impl Config {
                 }
                 Operation::AddPlayer(args[3].clone()) 
             },
+            "result" => {
+                if args.len() < 6 {
+                    return Err("not enough arguments for this command");
+                }
+                let result = match args[5].as_str() {
+                    "1-0" => MatchResult::WhiteWin,
+                    "0-1" => MatchResult::BlackWin,
+                    "0.5-0.5" => MatchResult::Draw,
+                    _ => return Err("unable to interpret match_result argument.")
+                };
+                Operation::Update {
+                    white_player_id: args[3].clone(),
+                    black_player_id: args[4].clone(),
+                    result: result
+                }
+            }
             "test::reset" => Operation::Reset,
             _ => return Err("unknown command"),
         };
@@ -45,23 +70,22 @@ impl Config {
 
 type Record = (String, u32);
 
-fn read_to_hashmap(filename: &str) -> Result<HashMap<String, u32>, Box<dyn Error>> {
+fn read_to_hashmap(filename: &str, data: &mut HashMap<String, u32>) -> Result<(), Box<dyn Error>> {
     let mut rdr = Reader::from_path(filename)?;
-    let mut data = HashMap::new();
 
     for result in rdr.deserialize() {
         let (player_id, rating): Record = result?;
-        data.insert(player_id, rating);
+        (*data).insert(player_id, rating);
     }
 
-    Ok(data)
+    Ok(())
 }
 
-fn write_to_csv(filename: &str, data: HashMap<String, u32>) -> Result<(), Box<dyn Error>> {
+fn write_to_csv(filename: &str, data: &mut HashMap<String, u32>) -> Result<(), Box<dyn Error>> {
     let mut wtr = Writer::from_path(filename)?;
 
     wtr.write_record(&["Player ID", "Rating"])?;
-    for (player_id, rating) in data.iter() {
+    for (player_id, rating) in (*data).iter() {
         let record = (player_id, rating);
         wtr.serialize(record)?;
         wtr.flush()?;
@@ -70,8 +94,8 @@ fn write_to_csv(filename: &str, data: HashMap<String, u32>) -> Result<(), Box<dy
     Ok(())
 }
 
-fn create_player(player_id: String, mut data: HashMap<String, u32>) -> Result<(), Box<dyn Error>> {
-    match data.entry(player_id) {
+fn create_player(player_id: String, data: &mut HashMap<String, u32>) -> Result<(), Box<dyn Error>> {
+    match (*data).entry(player_id) {
         Entry::Occupied(_) => {
            return Err("player_id already in use".into()) 
         }
@@ -83,24 +107,58 @@ fn create_player(player_id: String, mut data: HashMap<String, u32>) -> Result<()
     Ok(())
 }
 
+fn update_ratings(white_player_id: String, black_player_id: String, result: MatchResult, data: &mut HashMap<String, u32>) -> Result<(), Box<dyn Error>> {
+    let white_rating = match (*data).get(&white_player_id) {
+        Some(rat) => f64::from(*rat),
+        None => return Err("white player not found".into())
+    };
+    let black_rating = match (*data).get(&black_player_id) {
+        Some(rat) => f64::from(*rat),
+        None => return Err("black player not found".into())
+    };
+    let rating_difference = white_rating - black_rating;
+    let white_score_expected = 1. / (f64::exp(rating_difference / RATING_CONST) + 1.);
+    let black_score_expected = 1. - white_score_expected;
+
+    let (white_score, black_score) =  match result {
+        MatchResult::WhiteWin => (1., 0.),
+        MatchResult::BlackWin => (0., 1.),
+        MatchResult::Draw => (0.5, 0.5),
+    };
+
+    let white_new_rating = white_rating + K * (white_score - white_score_expected);
+    let black_new_rating = black_rating + K * (black_score - black_score_expected);
+    let white_new_rating = white_new_rating.round() as u32;
+    let black_new_rating = black_new_rating.round() as u32;
+    
+    (*data).insert(white_player_id, white_new_rating);
+    (*data).insert(black_player_id, black_new_rating);
+
+    Ok(())
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut data = read_to_hashmap(&config.filename)?;
     match config.operation {
         Operation::Help => {
             return Err("not implemented yet".into())
         }
-        Operation::Update{ .. } => {
-            return Err("not implemented yet".into())
+        Operation::Update{ white_player_id, black_player_id, result } => {
+            let mut data: HashMap<String, u32> = HashMap::new();
+            read_to_hashmap(&config.filename, &mut data)?;
+            update_ratings(white_player_id, black_player_id, result, &mut data)?; 
+            write_to_csv(&config.filename, &mut data)?;
         }
         Operation::AddPlayer(player_id) => {
-           create_player(player_id, data)?; 
-           // TODO write_to_csv(&config.filename, data)?;
+            let mut data: HashMap<String, u32> = HashMap::new();
+            read_to_hashmap(&config.filename, &mut data)?;
+            create_player(player_id, &mut data)?; 
+            write_to_csv(&config.filename, &mut data)?;
         }
         Operation::Reset => {
             let mut data = HashMap::new();
-            data.insert("Damian".to_string(), 1000);
-            data.insert("Daniel".to_string(), 800);
-            write_to_csv(&config.filename, data)?;
+            create_player("Damian".to_string(), &mut data)?;
+            create_player("Daniel".to_string(), &mut data)?;
+            write_to_csv(&config.filename, &mut data)?;
         }
     };
 
